974fcf510552ab32a2c8c88c020ae50f29b07ee6
Jonathan <67491744+SinnyHendrix@users.noreply.github.com>
2024-05-27 17:45:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var x = document.getElementById("skills");
+  var y = document.getElementById("member");
+  var z = document.getElementById("projects");
+  var a = document.getElementById("contact");
+  var b = document.getElementById("about"); 
